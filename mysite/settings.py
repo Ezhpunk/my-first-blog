cd255ae53830a665e2e49e2824b98b0fdc5e39f5
Maxim Tsikalenko <tsykalenkom@gmail.com>
2017-08-05 16:37:50 +0300

    Add views

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c+r%1#pjgf^hax5qrk#6q^k+qn01%l&5f(c_6)g$8x!kr2i=ts'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ezhpunk.pythonanywhere.com','localhost']
+ALLOWED_HOSTS = ['ezhpunk.pythonanywhere.com','localhost','127.0.0.1',]
 
 
 # Application definition
